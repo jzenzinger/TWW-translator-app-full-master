374accd7fe29606276ad55708870fedc14bddd81
Jiri Zenzinger <juris.zen@seznam.cz>
2021-04-08 14:45:25 +0200

    initial commit

--- js/main.js
+++ js/main.js
@@ -9,14 +9,25 @@ let history = [];
 var printing = '';
 var inputIsValid = false;
 
-function localHistory(input) {
+function localHistory(input, output) {
     //Check if localStorage is empty, if true, then set printing to NULL, cause it need to be mepty to display
     //  correct translation history
     //Add userInput into array of string and save it to localStorage
-    history.push(input);
+    history.push({input: input, output: output});
     console.log(history);
     //Display local storage item by item into printing string, use <br> after one element to display text 
     //  into div 
+
+    localStorage.setItem('history', JSON.stringify(this.history));
+}
+
+function displayStorage(history) {
+    var result = "";
+    history.forEach(function (item){
+        result += item.input + " -> " + item.output + "<br>";
+    });
+
+    document.getElementById("translationHistory").innerHTML = result;
 }
 
 //Checking function if input is not empty or string
@@ -64,8 +75,15 @@ button.onclick = function () {
             loadingDiv.style.display = 'none';
             // enable translate button
             button.removeAttribute('disabled');
-        });
 
-        localHistory(inputText);
+            
+            localHistory(inputText, resultInput.value);
+
+            if(localStorage.getItem('history')) {
+                this.history = JSON.parse(localStorage.getItem('history'));
+            }
+
+            displayStorage(history);
+        });
     }   
 }
